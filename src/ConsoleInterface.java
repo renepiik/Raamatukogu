359a27abb5330d510e0df7f0d6b8fc9d2a3cad2d
renepiik <rene@piik.eu>
2020-04-02 10:48:24 +0300

    Lisasin paar käsklust; osasid käsklusi (nt createBook) saab nüüd kutsuda otse käsurealt koos argumentidega

--- src/ConsoleInterface.java
+++ src/ConsoleInterface.java
@@ -9,8 +9,6 @@ public class ConsoleInterface {
 
     private String currentPath;
     private OperationType operationType = OperationType.DEFAULT;
-    private String currentLibraryName;
-    private String currentBookName;
 
     private ArrayList<Library> libraries;
     private Library selectedLibrary;
@@ -19,7 +17,7 @@ public class ConsoleInterface {
     private static ConsoleInterface instance = null;
     private static Scanner scanner;
 
-    private ConsoleInterface() throws Exception {
+    private ConsoleInterface() {
         this.currentPath = "";
         scanner = new Scanner(System.in);
         // should read libraries from a local file here
@@ -37,23 +35,21 @@ public class ConsoleInterface {
 
         // if operationtype is set to a library
         if (newType == OperationType.LIBRARY) {
-            this.currentLibraryName = newPath;
             this.currentPath = newPath;
         } else if (newType == OperationType.BOOK && selectedLibrary != null) {
             this.currentPath = this.selectedLibrary.getName()+"/"+newPath;
-            this.currentBookName = newPath.substring(newPath.lastIndexOf('/')+1);
         }
     }
 
     public String getCommand(String query) {
         System.out.println(query);
-        System.out.print("~/"+this.currentPath+" ");
+        System.out.print("~/"+this.currentPath+"$ ");
         return scanner.nextLine();
     }
 
     public boolean quit() throws IOException {
         String input = this.getCommand("Kas te soovite programmist lahkuda? (jah/ei)");
-        if (input.equals("jah") || input.equals("JAH")) {
+        if (input.toLowerCase().equals("jah")) {
             // siin salvestatakse kõik sisestatud info
             for (Library library : this.libraries) {
                 library.save();
@@ -64,12 +60,23 @@ public class ConsoleInterface {
         return false;
     }
 
+    public void list() {
+        if (this.operationType != OperationType.DEFAULT) {
+            listBooks();
+        }
+        else {
+            listLibraries();
+        }
+    }
+
     /*
     LIBRARY METHODS
 
     Methods accessible from root:
         - selectLibrary()
+        - selectLibraryWithName(String libName)
         - createLibrary()
+        - createLibraryWithName(String libName)
         - listLibraries()
 
     Methods accessible when this.selectedLibrary has been set:
@@ -79,8 +86,12 @@ public class ConsoleInterface {
      */
 
     public void selectLibrary() {
-        boolean canOpen = false;
         String libName = this.getCommand( "Sisesatage raamatukogu nimi: ");
+        selectLibraryWithName(libName);
+    }
+
+    public void selectLibraryWithName(String libName) {
+        boolean canOpen = false;
         Library selectedLibrary = null;
 
         // check if library with given name exists
@@ -100,9 +111,12 @@ public class ConsoleInterface {
     }
 
     public void createLibrary() {
-        boolean canCreate = true;
-
         String libName = this.getCommand("Sisestage loodava raamatukogu nimi: ");
+        createLibraryWithName(libName);
+    }
+
+    public void createLibraryWithName(String libName) {
+        boolean canCreate = true;
 
         // check if library with same name already exists
         for (Library library : this.libraries) {
@@ -127,10 +141,10 @@ public class ConsoleInterface {
     // these methods can only be accessed when this.selectedLibrary has been set
 
     public void deleteLibrary() {
-        if (this.selectedLibrary != null) {
+        if (this.operationType == OperationType.LIBRARY) {
             String deleteCommand = this.getCommand("Kas te soovite eemaldada raamatukogu '"+this.selectedLibrary.getName()+"'? (jah/ei)");
 
-            if (deleteCommand.equals("jah") || deleteCommand.equals("JAH")) {
+            if (deleteCommand.toLowerCase().equals("jah")) {
                 this.libraries.removeIf(library -> library.getName().equals(this.selectedLibrary.getName()));
             }
 
@@ -139,10 +153,27 @@ public class ConsoleInterface {
         }
     }
 
-    // TODO
     public void updateLibrary() {
-        if (this.selectedLibrary != null) {
+        if (this.operationType == OperationType.LIBRARY) {
+            String newName = this.getCommand("Sisesta uus nimi raamatukogule: ");
 
+            boolean canChange = true;
+
+            // check if library with same name already exists
+            for (Library library : this.libraries) {
+                if (library.getName().equals(newName)) {
+                    canChange = false;
+                    break;
+                }
+            }
+
+            if (canChange) {
+                this.selectedLibrary.setName(newName);
+                this.updatePath(this.selectedLibrary.getName(), OperationType.LIBRARY);
+                System.out.println("Raamatukogu uus nimi on "+newName);
+            } else {
+                System.out.println("Tekkis tõrge, sellise nimega raamatukogu on juba olemas.");
+            }
         }
     }
 
@@ -150,27 +181,37 @@ public class ConsoleInterface {
     BOOK METHODS
 
     Methods accessible when this.selectedLibrary has been set:
-        - selectLibrary()
-        - createLibrary()
-        - listLibraries()
+        - selectBook()
+        - selectBookWithTitle(String bookTitle)
+        - createBook()
+        - createBookWithTitleAndAuthor(String title, String author)
+        - listBooks()
+        - getRandomBook()
 
     Methods accessible when this.selectedBook has been set:
-        - updateLibrary()
-        - deleteLibrary()
+        - updateBook()
+        - deleteBook()
+        - displayBookInfo()
      */
 
     // asks for book title, then gives a selection of all the books with given title
     // user enters a number corresponding to the book to select it
     public void selectBook() {
-        if (this.selectedLibrary != null) {
+        if (this.operationType == OperationType.LIBRARY) {
+            String bookTitle = this.getCommand("Sisestage raamatu pealkiri: ");
+            selectBookWithTitle(bookTitle);
+        }
+    }
+
+    public void selectBookWithTitle(String bookTitle) {
+        if (this.operationType == OperationType.LIBRARY) {
             boolean canOpen = false;
-            String bookName = this.getCommand("Sisestage raamatu pealkiri: ");
             Book selectedBook;
             ArrayList<Book> booksWithSameTitle = new ArrayList<>();
 
             // check if book with given name exists
             for (Book book : this.selectedLibrary.getBooks()) {
-                if (book.getTitle().equals(bookName)) {
+                if (book.getTitle().toLowerCase().equals(bookTitle.toLowerCase())) {
                     canOpen = true;
                     booksWithSameTitle.add(book);
                 }
@@ -178,7 +219,8 @@ public class ConsoleInterface {
 
             if (canOpen) {
                 if (booksWithSameTitle.size() == 1) {
-                    updatePath(bookName, OperationType.BOOK);
+                    this.updatePath(bookTitle, OperationType.BOOK);
+                    this.selectedBook = booksWithSameTitle.get(0);
                 }
                 else {
                     System.out.println("Millise raamatu te soovite avada? ");
@@ -196,40 +238,56 @@ public class ConsoleInterface {
     }
 
     public void createBook() throws Exception {
-        if (this.selectedLibrary != null) {
-            boolean canCreate = true;
-
+        if (this.operationType == OperationType.LIBRARY) {
+            boolean create = true;
             String bookName = this.getCommand("Sisestage loodava raamatu pealkiri: ");
 
-            // check if book with same name already exists
+            // if there already is another book with the same title, asks for confirmation
             for (Book book : this.selectedLibrary.getBooks()) {
                 if (book.getTitle().equals(bookName)) {
-                    canCreate = false;
+                    create = this.getCommand("Sellise pealkirjaga raamat on juba olemas. Kas soovid luua uut? (jah/ei)").toLowerCase().equals("jah");
                     break;
                 }
             }
 
             String authorName = this.getCommand("Sisestage raamatu autori nimi: ");
 
-            if (canCreate) {
-                this.selectedLibrary.addBook(new Book(bookName, authorName));
-                System.out.println("Uus raamat pealkirjaga "+bookName+" ja autoriga "+authorName+" on loodud");
-            } else {
-                System.out.println("Tekkis tõrge, sellise pealkirjaga raamat on juba olemas");
+            if (create) {
+                this.createBookWithTitleAndAuthor(bookName, authorName);
             }
         }
     }
 
+    public void createBookWithTitleAndAuthor(String title, String author) throws Exception {
+        if (this.operationType == OperationType.LIBRARY) {
+            this.selectedLibrary.addBook(new Book(title, author));
+            System.out.println("Uus raamat pealkirjaga "+title+" ja autoriga "+author+" on loodud");
+        }
+    }
+
     public void listBooks() {
-        if (this.selectedLibrary != null) {
+        if (this.operationType == OperationType.LIBRARY) {
             for (Book book : this.selectedLibrary.getBooks()) {
                 System.out.println(book);
             }
         }
     }
 
+    // Math.Random funktsiooni kasutus
+    public void getRandomBook() {
+        if (this.operationType == OperationType.LIBRARY) {
+            int bookIndex = (int) Math.floor(Math.random() * this.selectedLibrary.getBooks().size());
+            System.out.println(this.selectedLibrary.getBooks().get(bookIndex));
+        }
+        else {
+            System.out.println("Raamatukogu pole valitud");
+        }
+    }
+
+    // these methods can only be accessed when this.selectedBook has been set
+
     public void deleteBook() {
-        if (this.selectedLibrary != null && this.selectedBook != null) {
+        if (this.operationType == OperationType.BOOK) {
             boolean canDelete = false;
             ArrayList<Book> booksWithSameTitle = new ArrayList<>();
             String bookName = this.getCommand("Sisestage raamatu pealkiri, mille soovite eemaldada: ");
@@ -246,7 +304,7 @@ public class ConsoleInterface {
                 if (booksWithSameTitle.size() == 1) {
                     String deleteCommand = this.getCommand("Kas te soovite eemaldada raamatu pealkirjaga '"+bookName+"'? (jah/ei)");
 
-                    if (deleteCommand.equals("jah") || deleteCommand.equals("JAH")) {
+                    if (deleteCommand.toLowerCase().equals("jah")) {
                         this.selectedLibrary.getBooks().removeIf(book -> book.getTitle().equals(bookName));
                     }
                 }
@@ -259,7 +317,7 @@ public class ConsoleInterface {
                     int bookIndex = Integer.parseInt(this.getCommand("Sisestage raamatu ees olev number siia: "));
                     String deleteCommand = this.getCommand("Kas te soovite eemaldada raamatu '"+bookIndex+": "+booksWithSameTitle.get(bookIndex)+"'? (jah/ei)");
 
-                    if (deleteCommand.equals("jah") || deleteCommand.equals("JAH")) {
+                    if (deleteCommand.toLowerCase().equals("jah")) {
                         this.selectedLibrary.removeBook(booksWithSameTitle.get(bookIndex));
                         this.selectedBook = null;
                         this.updatePath(this.selectedLibrary.getName(), OperationType.LIBRARY);
@@ -271,17 +329,46 @@ public class ConsoleInterface {
         }
     }
 
-    // TODO
-    public void updateBook() {
+    public void updateBook() throws Exception {
+        if (this.operationType == OperationType.BOOK) {
+            System.out.println("Iga küsimuse juures võid vajutada enter, kui soovid välja muutmata jätta.");
 
-    }
+            // title
+            String newTitle = this.getCommand("Sisesta pealkiri: ");
+            if (!newTitle.equals("")) this.selectedBook.setTitle(newTitle);
 
-    // Math.Random funktsiooni kasutus
-    public Book getRandomBook() throws Exception {
-        if (this.selectedLibrary != null) {
-            int bookIndex = (int) Math.floor(Math.random() * this.selectedLibrary.getBooks().size());
-            return this.selectedLibrary.getBooks().get(bookIndex);
+            // authorName
+            String newAuthorName = this.getCommand("Sisesta autori nimi: ");
+            if (!newAuthorName.equals("")) this.selectedBook.setAuthorName(newAuthorName);
+
+            // publicationDate
+            String newPublicationDate = this.getCommand("Sisesta ilmumisaasta: ");
+            if (!newPublicationDate.equals("")) this.selectedBook.setPublicationDate(newPublicationDate);
+
+            // genre
+            String newGenre = this.getCommand("Sisesta žanr: ");
+            if (!newGenre.equals("")) this.selectedBook.setGenre(newGenre);
+
+            // ISBN
+            String newISBN = this.getCommand("Sisesta ISBN: ");
+            if (!newISBN.equals("")) this.selectedBook.setISBN(newISBN);
+
+            // status
+            String newStatus = this.getCommand("Sisesta staatus (DEFAULT, LENT, READING): ");
+            if (!newStatus.equals("")) this.selectedBook.setStatus(Status.valueOf(newStatus.toUpperCase()));
+
+            System.out.println("Raamatu info uuendatud! ");
+            System.out.println(this.selectedBook);
         }
-        return new Book("", "");
+        else {
+            System.out.println("Vali enne raamat, mida soovid uuendada");
+        }
     }
+
+    public void displayBookInfo() {
+        if (this.operationType == OperationType.BOOK) {
+            System.out.println(this.selectedBook);
+        }
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -6,26 +6,34 @@ public class Main {
     public static void main(String[] args) throws Exception {
         boolean runProgram = true;
         ArrayList<Library> libraries = new ArrayList<>();
+        Map<String, String> commands = new HashMap<>();
+        ConsoleInterface console = ConsoleInterface.getInstance();
+
+        // ingliskeelsed juhised programmi kasutamiseks
+        commands.put("list, ls", "list based on context");
+        commands.put("ll", "list libraries");
+        commands.put("sl <name>", "select library");
+        commands.put("cl <name>", "create library");
+        commands.put("dl", "delete library");
+        commands.put("ul", "update library");
+        commands.put("lb", "list books");
+        commands.put("sb", "select book");
+        commands.put("cb <title> -a <author>", "create book");
+        commands.put("db", "delete book");
+        commands.put("rb", "random book");
+        commands.put("ub", "update book");
+        commands.put("vb", "view book");
+        commands.put("quit", "quit program");
 
         while (runProgram) {
-            Map<String, String> commands = new HashMap<>();
-            ConsoleInterface console = ConsoleInterface.getInstance();
-          
-            commands.put("ll", "list libraries");
-            commands.put("cl", "create library");
-            commands.put("dl", "delete library");
-            commands.put("sl", "select library");
-            commands.put("quit", "quit program");
-            commands.put("lb", "list books");
-            commands.put("sb", "select book");
-            commands.put("cb", "create book");
-            commands.put("db", "delete book");
+            String[] userInput = console.getCommand("Sisesta käsklus või 'help' abi jaoks: ").split(" ");
 
-            String userInput = console.getCommand("Sisesta käsklus või 'help' abi jaoks: ");
-
-            switch (userInput) {
+            switch (userInput[0]) {
                 case "help": {
-                    System.out.println(commands);
+                    // kuvab iga käskluse uuel real
+                    for (Map.Entry<String, String> stringStringEntry : commands.entrySet()) {
+                        System.out.println(stringStringEntry.getKey()+": "+stringStringEntry.getValue());
+                    }
                     break;
                 }
                 case "quit": {
@@ -33,13 +41,22 @@ public class Main {
                         runProgram = false;
                         break;
                     }
+                    break;
                 }
                 case "ll": { // list libraries
                     console.listLibraries();
                     break;
                 }
                 case "cl": { // create library
-                    console.createLibrary();
+                    if (userInput.length > 1) {
+                        StringBuilder name = new StringBuilder();
+                        for (int i = 1; i < userInput.length; i++) {
+                            name.append(userInput[i]).append(" ");
+                        }
+                        name.deleteCharAt(name.length()-1);
+                        console.createLibraryWithName(name.toString());
+                    }
+                    else console.createLibrary();
                     break;
                 }
                 case "dl": { // delete library
@@ -47,11 +64,54 @@ public class Main {
                     break;
                 }
                 case "sl": { // select library
-                    console.selectLibrary();
+                    if (userInput.length > 1) {
+                        StringBuilder name = new StringBuilder();
+                        for (int i = 1; i < userInput.length; i++) {
+                            name.append(userInput[i]).append(" ");
+                        }
+                        name.deleteCharAt(name.length()-1);
+                        console.selectLibraryWithName(name.toString());
+                    }
+                    else console.selectLibrary();
+                    break;
+                }
+                case "ul": {
+                    console.updateLibrary();
                     break;
                 }
                 case "sb": {
                     console.selectBook();
+                    break;
+                }
+                case "cb": {
+                    if (userInput.length > 1) {
+                        // find the index of String "-a" in userInput[]
+                        int cIndex = 1;
+
+                        for (int i = 1; i < userInput.length; i++) {
+                            if (userInput[i].equals("-a")) cIndex = i;
+                        }
+
+                        // join the Strings between 'cb' and '-' to title
+                        StringBuilder title = new StringBuilder();
+
+                        for (int i = 1; i < cIndex; i++) {
+                            title.append(userInput[i]).append(" ");
+                        }
+                        title.deleteCharAt(title.length()-1);
+
+                        // join the Strings after '-' to author
+                        StringBuilder authorName = new StringBuilder();
+
+                        for (int i = cIndex+1; i < userInput.length; i++) {
+                            authorName.append(userInput[i]).append(" ");
+                        }
+                        authorName.deleteCharAt(authorName.length()-1);
+
+                        // create book
+                        console.createBookWithTitleAndAuthor(title.toString(), authorName.toString());
+                    }
+                    else console.createBook();
                     break;
                 }
                 case "lb": {
@@ -62,12 +122,25 @@ public class Main {
                     console.deleteBook();
                     break;
                 }
-                case "cb": {
-                    console.createBook();
+                case "rb": {
+                    console.getRandomBook();
                     break;
                 }
-                case "rb": {
-                    System.out.println(console.getRandomBook());
+                case "ub": {
+                    console.updateBook();
+                    break;
+                }
+                case "list":
+                case "ls": {
+                    console.list();
+                    break;
+                }
+                case "vb": {
+                    console.displayBookInfo();
+                    break;
+                }
+                default: {
+                    System.out.println("Sellist käsklust pole.");
                     break;
                 }
             }
