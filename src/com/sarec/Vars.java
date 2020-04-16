e5798729783e78bb31c1c070ee7cd925f287be1d
rene <rene@piik.eu>
2020-04-16 10:02:37 +0300

    Lisasin Vars.java faili värvid ja resources/ kausta ikoonid programmis kasutamiseks

--- src/com/sarec/Vars.java
+++ src/com/sarec/Vars.java
@@ -1,8 +1,29 @@
 package com.sarec;
 
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+
 import javax.swing.filechooser.FileSystemView;
 
 public class Vars {
     // kaust, kus hoitakse raamatukogude infot
     public static String libsPath = FileSystemView.getFileSystemView().getDefaultDirectory().getPath()+"\\libs\\";
+
+    public static String ButtonColor = "#6246ea";
+    public static Paint ButtonPaint = Color.web(ButtonColor);
+
+    public static String BackgroundColor = "#fffffe";
+    public static Paint BackgroundPaint = Color.web(BackgroundColor);
+
+    public static String DarkTextColor = "#2b2c34";
+    public static Paint DarkTextPaint = Color.web(DarkTextColor);
+
+    public static String LightTextColor = "#fffffe";
+    public static Paint LightTextPaint = Color.web(LightTextColor);
+
+    public static String SecondaryColor = "#d1d1e9";
+    public static Paint SecondaryPaint = Color.web(SecondaryColor);
+
+    public static String TertiaryColor = "#e45858";
+    public static Paint TertiaryPaint = Color.web(TertiaryColor);
 }
